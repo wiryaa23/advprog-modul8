1afd184c5637ae858525645fecb0098bf1d004d0
wiryaa23 <wiryawdk@gmail.com>
2025-05-06 00:55:29 +0700

    Refactor base skeleton of Rust gRPC.

--- src/grpc_client.rs
+++
@@ -1,9 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-     tonic_build::configure()
-         .build_server(true)
-         .compile(
-             &["proto/services.proto"],
-             &["proto"],
-         )?;
-     Ok(())
-}
